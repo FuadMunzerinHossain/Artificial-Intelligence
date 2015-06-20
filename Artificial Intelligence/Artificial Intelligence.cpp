a9fb62c3ec1372691afb49dab105a058a2317a67
Fuad Munzerin Hossain <Fuad.Hossain@mail.utoronto.ca>
2015-06-20 03:13:49 -0400

    Started the AI logic, AI now asks basic questions

--- Artificial Intelligence/Artificial Intelligence.cpp
+++ Artificial Intelligence/Artificial Intelligence.cpp
@@ -3,16 +3,24 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int i = 0;
-	while (i == 0)
-	cout << "Hello" << endl;
-	int j = 0;
-	cin >>j ;
+	string userName;
+	string command;
+	cout << "Hi, What is your name? "<<endl;
+	getline(std::cin, userName);
+	cout << endl;
+
+	cout << "It's nice to meet you " << userName << "!" << endl;
+	cout << endl;
+	cout << "How may I help you?" << endl;
+	getline(std::cin, command);
+	cout << endl;
+
 	return 0;
 }
 
