de5553c94bb03596451a8fe0b30632bd7808a5a2
Fuad Munzerin Hossain <Fuad.Hossain@mail.utoronto.ca>
2015-06-20 15:37:18 -0400

    Added the loop structure, implemented the first command (reset)

--- Artificial Intelligence/Artificial Intelligence.cpp
+++ Artificial Intelligence/Artificial Intelligence.cpp
@@ -2,25 +2,48 @@
 //
 
 #include "stdafx.h"
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+//int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, const char* argv[])
 {
 	string userName;
 	string command;
-	cout << "Hi, What is your name? "<<endl;
-	getline(std::cin, userName);
-	cout << endl;
+	bool newAI = true;
+	bool exit = false;
+	
+	//keep looping until the user wants to end the program
+	while (exit == false)
+	{
+		//this if statement executes when the AI program first starts, and also if the user wishes to reset the AI
+		if (newAI == true)
+		{
+			cout << "New AI initializing..." << endl;
+			cout << "Hi, What is your name? " << endl;
+			getline(std::cin, userName);
+			cout << endl;
 
-	cout << "It's nice to meet you " << userName << "!" << endl;
-	cout << endl;
-	cout << "How may I help you?" << endl;
-	getline(std::cin, command);
-	cout << endl;
+			cout << "It's nice to meet you " << userName << "!" << endl;
+			cout << endl;
 
+			newAI = false;
+		}
+
+		//get the command from the user
+		cout << "How may I help you?" << endl;
+		getline(std::cin, command);
+		cout << endl;
+
+		if (command == "reset")
+		{
+			newAI = true;
+		}
+
+	}
 	return 0;
 }
 
