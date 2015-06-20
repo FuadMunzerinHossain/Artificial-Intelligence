a52e68b0e2a8cdcbf3f1858ba2d13b3d0addab61
Fuad Munzerin Hossain <Fuad.Hossain@mail.utoronto.ca>
2015-06-20 01:41:17 -0400

    Initial commit

---
+++ Artificial Intelligence/Artificial Intelligence.cpp
@@ -0,0 +1,18 @@
+// Artificial Intelligence.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i = 0;
+	while (i == 0)
+	cout << "Hello" << endl;
+	int j = 0;
+	cin >>j ;
+	return 0;
+}
+
