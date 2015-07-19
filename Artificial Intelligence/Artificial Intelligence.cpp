2beedf5ca005793d8915d05bd7b6e06393774a2b
Fuad Munzerin Hossain <Fuad.Hossain@mail.utoronto.ca>
2015-07-19 16:33:33 -0400

    Major Code Restructure/Cleanup; made functions pass by reference instead of value, minimized the amount of code in the main function, main's helper functions are all in a separate file now

--- Artificial Intelligence/Artificial Intelligence.cpp
+++ Artificial Intelligence/Artificial Intelligence.cpp
@@ -1,41 +1,26 @@
 // Artificial Intelligence.cpp : Defines the entry point for the console application.
 // CONTAINS MAIN FUNCTION
-//
 
-#include "stdio.h"
-#include "MakeLowerCase.h"
-#include "TicTacToe.h"
-#include "IsValidCommand.h"
-#include "Parser_main.h"
+#include "MainHelper.h"
 
-#include <string>
-#include <vector>
-#include <iostream>
-#include <time.h>
-
-using namespace std;
+int playerWins;
+int aiWins;
 
 int main(int argc, const char* argv[])
 {
-	string userName;
 	string command;
 	string commandAdjusted;
-	bool newAI = true;
 	bool exit = false;
 	clock_t start;
-	int playerWins = 0;
-	int aiWins = 0;
-	start = clock();	// sets the start time at now. 
+	string userName;
 
-
-	// Introduction for the first time: 
-	userName = resetAI(); 
+	// Player initial introductions
+	resetAI(playerWins, aiWins, start, userName); 
 	
-	//keep looping until the user wants to end the program
+	// Keep looping until the user wants to end the program
 	while (exit == false)
 	{
-
-		//get the command from the user
+		// Get the command from the user
 		cout << endl;
 		cout << "How may I help you?" << endl;
 		getline(std::cin, command);
@@ -45,10 +30,7 @@ int main(int argc, const char* argv[])
 
 		if (commandAdjusted == "reset")
 		{
-			playerWins = 0;
-			aiWins = 0;
-			start = clock(); // reset start time 
-			userName = resetAI(); 
+			resetAI(playerWins, aiWins, start, userName);
 		}
 
 		else if (commandAdjusted == "summary")
@@ -56,66 +38,26 @@ int main(int argc, const char* argv[])
 			showSummary(start,  playerWins, aiWins);
 		}
 
-		else if (commandAdjusted == "exit"){
-			exit = true;
-			showSummary(start, playerWins, aiWins);
-			cout << "Thanks for playing! :)" << endl;
-		} 
-
-		else if (commandAdjusted == "help"){
-			showHelp(); 
-		}
-
 		else if (commandAdjusted == "game" || commandAdjusted == "play")
 		{
-			// The program enters the state to play game. 
-			// The logic here should be isolated away from main func.
-
-			string gameSelect;
-			string gameSelectAdjusted;
-
-			cout << "What game do you want to play? (Type in the number or the name of the game)" << endl;
-			cout << "1. Tic-tac-toe" << endl;
-			getline(std::cin, gameSelect);
-			gameSelectAdjusted = makeLowerCase(gameSelect);
-
-			//checks if argument is valid
-			while (!isValidCommand(gameSelectAdjusted)){
-				cout << endl;
-				cout << "Please enter a valid argument." << endl;
-				getline(std::cin, gameSelect);
-				gameSelectAdjusted = makeLowerCase(gameSelect);
-			}		
-
-			if (gameSelectAdjusted == "1" || gameSelectAdjusted == "tic-tac-toe")
-			{
-				string firstTurn;
-				string firstTurnAdjusted;
-
-				cout << "Do you want to go first? (Y/N)" << endl;
-				getline(std::cin, firstTurn);
-				cout << endl;
-
-				firstTurnAdjusted = makeLowerCase(firstTurn);
-
-				if (firstTurnAdjusted == "y")
-				{
-					tictactoe(true);
-				}
-
-				else
-				{
-					tictactoe(false);
-				}
-			}
+			playGame();
 		}
-		else {	// invalid command 
-			cout << "Invalid Command!\n"; 
+
+		else if (commandAdjusted == "help")
+		{
+			showHelp();
+		}
+
+		else if (commandAdjusted == "exit")
+		{
+			exitAI(exit, start, playerWins, aiWins);
+		}
+
+		// invalid command
+		else 
+		{
 			showHelp(); 
 		}
-
-
-
 	}
 	return 0;
 }
